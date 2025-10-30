1a68a2f2dd3fdf75cad75129977c8a048d3218d1
Sameer Shah <sameershah9167@gmail.com>
2025-10-31 01:31:20 +0530

    Edit

--- BackgroundWorker/Program.cs
+++ BackgroundWorker/Program.cs
@@ -9,8 +9,10 @@ builder.Services.AddHostedService<CombinationInsertWorker>();
 
 var configuration = builder.Configuration;
 var connectionString = configuration.GetConnectionString("DefaultConnection");
-builder.Services.AddDbContext<DBContext>(options => options.UseSqlServer(connectionString));
+//builder.Services.AddDbContext<DBContext>(options => options.UseSqlServer(connectionString));
 
+builder.Services.AddDbContext<DBContext>(options =>
+    options.UseMySql(connectionString, ServerVersion.AutoDetect(connectionString)));
 
 
 var host = builder.Build();
